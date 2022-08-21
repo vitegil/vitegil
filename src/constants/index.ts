9b93bdc5c6fcdfd3d4d69ff5fd83089cef927fa6
Angelna <1443329043@qq.com>
2022-08-22 01:19:45 +0800

    feat(router): 路由守卫

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,10 @@
 export const storageKey = {
   appId: 'VITEGIL_APP_ID',
 }
+
+export const env = {
+  isDev:
+    import.meta.env.MODE === 'development' || import.meta.env.MODE === 'mock',
+  isStrictDev: import.meta.env.MODE === 'development',
+  isStrictMock: import.meta.env.MODE === 'mock',
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import type { RouteRecordRaw } from 'vue-router'
 import { createRouter, createWebHistory } from 'vue-router'
+import { env, storageKey } from '@/constants'
 
 const monitorRoutes: RouteRecordRaw[] = [
   {
@@ -30,14 +31,37 @@ export const fullScreenRoutes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('@/views/Login/index.vue'),
   },
-  {
+]
+
+if (env.isDev) {
+  fullScreenRoutes.push({
     name: 'Demo',
     path: '/demo',
     component: () => import('@/views/Demo/index.vue'),
-  },
-]
+    children: monitorRoutes,
+  })
+}
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes: [...fullScreenRoutes, ...monitorRoutes],
 })
+
+router.beforeEach((to, from, next) => {
+  // 暂时使用 appId 表示登录状态
+  const isLogined = localStorage.getItem(storageKey.appId)
+  // 如果已登陆，放行
+  if (isLogined) {
+    next()
+    return
+  }
+  // 如果未登录，但目标路由是 public 页面，放行
+  if (fullScreenRoutes.find(item => item.path === to.path)) {
+    next()
+    return
+  }
+  // 如果未登录，且目标路由不是 public 页面，跳转到登录页
+  next({ name: 'Login' })
+})
+
+export default router
